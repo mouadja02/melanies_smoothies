2e0d76e11fe60875101546e1601419d60e3efe77
mouadja02 <118407337+mouadja02@users.noreply.github.com>
2025-02-11 17:06:04 +0100

    Update streamlit_app.py
    Putting the JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ name_on_order = st.text_input("Name on the smoothie")
 st.write("The name on your smoothie will be", name_on_order)
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_fd =st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
